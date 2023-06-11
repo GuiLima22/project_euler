13c5c6840720d41f643b1ae2bd39897ed853adc0
Luis Guilherme Lima <luisglimasouza@hotmail.com>
2023-06-11 20:28:50 -0300

    .

--- problem_0002/script.js
+++ problem_0002/script.js
@@ -1,15 +1,20 @@
-let i = 0
-let s = 0
-let f1 = 1
-let f2 = 2
-
-console.log(f1)
-console.log(f2)
-
-while(i < 10){
-    s = f1 + f2
-    f1 = s - f2
-    f2 = s
-    i++
-    console.log(s);
+function million(x){
+    return x*10**6
 }
+
+let i = 0;
+let f1 = 1;
+let f2 = 1;
+let sum = 0;
+const a = million(4);
+
+while(i < a){
+    i = f1 + f2;
+    f1 = i - f1;
+    f2 = i;
+    if (i % 2 == 0 && i < a){
+        sum += i;
+    }
+}
+
+console.log(sum);
