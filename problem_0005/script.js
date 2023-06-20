186890342839298566e90e22e46b53459c07f55f
Luis Guilherme Lima <luisglimasouza@hotmail.com>
2023-06-19 21:33:14 -0300

    .

--- problem_0005/script.js
+++ problem_0005/script.js
@@ -6,7 +6,7 @@ let i = 0;
 let arr = [];
 const divisors = [];
 
-while(i < 10){
+while(i < 20){
     i++
     divisors.push(i)
 };
@@ -15,8 +15,8 @@ console.log(divisors)
 
 i = 0
 
-/*while(i > -1){
-    i += 2
+while(true){
+    i += 2520
     for (const divisor in divisors){
         const quocient = round(i / divisors[divisor])
         if (quocient % 1 == 0){
@@ -28,9 +28,11 @@ i = 0
     }
 
     arr = []
-}*/
+}
+console.log(arr)
+console.log(i)
 
-for (const divisor in divisors){
+/*for (const divisor in divisors){
     if(2520 % divisors[divisor] != 0){
         let a = false
     }
@@ -40,7 +42,38 @@ for (const divisor in divisors){
 }
 
 while(a == true){
-    
+    if(){
+
+    }
 }
 
-arr = []
+arr = []*/
+
+/*function smallest(valor, divisores){
+    for (const i in divisores){
+        if (valor % i != 0){
+            let a = false
+        }
+        else{
+            a = true
+        }
+    }
+    while (a == true){
+        if (smallest(valor, divisores) == false){
+            valor++
+        }
+    }
+}
+
+console.log(smallest(0, divisors))
+
+def smallest(valor):
+    for i in range(0,21):
+        if valor % i != 0:
+            a = False
+        else:
+            a = True
+    while a == True:
+        if smallest(valor):
+            break
+            valor += 1*/ 
