165f6e75c32b48c83179b3b3428e864e078e08c1
Luis Guilherme Lima <luisglimasouza@hotmail.com>
2023-06-29 15:50:25 -0300

    .

---
+++ problem_0009/script.js
@@ -0,0 +1,35 @@
+function round(number) {
+    return Math.round(number * 100) / 100
+}
+
+function sqrt(number) {
+    Math.sqrt(number)
+}
+
+let a = 1
+let b = 1
+// let c = 1
+
+// while(a < 1000){
+//     a++;
+//     b = a;
+//     c = b;
+//     while (b < 1000) {
+//         b++;
+//         c = b;
+//         while (c < 1000){
+//             c++;
+//             if (a**2 + b**2 == c**2 && a + b + c == 1000) {
+//                 console.log(a*b*c);
+//                 break;
+//             }
+//         }
+//         if (a**2 + b**2 == c**2 && a + b + c == 1000) {
+//             break;
+//         }
+//     }
+//     if (a**2 + b**2 == c**2 && a + b + c == 1000) {
+//         break;
+//     }
+// }
+
